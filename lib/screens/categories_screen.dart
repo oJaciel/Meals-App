ebafa103a5d3fa42ccb81f61e39c5ec10d166ea1
oJaciel <jacielhbarichellO@gmail.com>
2025-01-01 21:11:59 -0300

    Tela de categorias
    Criada a tela das categorias e registrada como a tela inicial da aplicação.
    
    Criado componente para cada categoria mostrada na tela.

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar?'),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200, //Extensão de cada elemento
+          childAspectRatio: 3 / 2, //Proporção de cada elemento
+          crossAxisSpacing: 20, //Espaçamento horizontal entre cada elemento
+          mainAxisSpacing: 20, //Espaçamento vertical entre cada elemento
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
+    );
+  }
+}
