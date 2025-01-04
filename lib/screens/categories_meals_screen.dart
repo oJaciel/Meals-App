47cebf55689ea6ef51605a3b649fdc54a4436ad8
oJaciel <jacielhbarichellO@gmail.com>
2025-01-04 16:40:07 -0300

    Mostrando receitas na tela da categoria

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import '../models/category.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   const CategoriesMealsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //Recebendo a categoria via construtor do Navigator
     final category = ModalRoute.of(context)?.settings.arguments as Category;
+
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category
+          .id); 
+          //Se id da categoria está dentro da categoria da comida, add na lista
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por Categorias ${category.id}'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
